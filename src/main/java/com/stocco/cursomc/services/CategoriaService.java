fbe8771bb33a15a67d7b9decef36d0822c703eb5
Gabriel Stocco <piresstocco.gabriel@gmail.com>
2020-02-05 15:37:38 -0300

    Deletenado uma Categoria com DELETE

--- src/main/java/com/stocco/cursomc/services/CategoriaService.java
+++ src/main/java/com/stocco/cursomc/services/CategoriaService.java
@@ -1,10 +1,12 @@
 package com.stocco.cursomc.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.stocco.cursomc.domain.Categoria;
 import com.stocco.cursomc.repositories.CategoriaRepository;
+import com.stocco.cursomc.services.exceptions.DataIntegrityException;
 import com.stocco.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -31,6 +33,16 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.delete(id);
+	
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possivel excluir uma categoria que possui produtos");
+		}
 	}
 	
 }
