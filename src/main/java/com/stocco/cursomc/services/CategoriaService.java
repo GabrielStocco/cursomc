43a093db04e439b54c4bcaa415c9e774050b77a4
Gabrie Stocco <gpires@indracompany.com>
2020-02-13 11:03:56 -0300

    Validacao sintatica com Bean Validation

--- src/main/java/com/stocco/cursomc/services/CategoriaService.java
+++ src/main/java/com/stocco/cursomc/services/CategoriaService.java
@@ -10,6 +10,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.stocco.cursomc.domain.Categoria;
+import com.stocco.cursomc.dto.CategoriaDTO;
 import com.stocco.cursomc.repositories.CategoriaRepository;
 import com.stocco.cursomc.services.exceptions.DataIntegrityException;
 import com.stocco.cursomc.services.exceptions.ObjectNotFoundException;
@@ -57,6 +58,10 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
 		PageRequest pageRequest = new PageRequest(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 	
 }
